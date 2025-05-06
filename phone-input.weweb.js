422e3389f6be780bc576248b56fc7636f941ab05
felipeclimbup <136645684+felipeclimbup@users.noreply.github.com>
2025-05-06 16:12:15 -0400

    Add files via upload

---
+++ phone-input.weweb.js
@@ -0,0 +1,52 @@
+export default {
+  props: {
+    modelValue: String,
+  },
+  emits: ["update:modelValue", "update:country"],
+  data() {
+    return {
+      countries: [],
+      selectedCountryCode: "CL",
+      phone: "",
+      currentMask: "",
+    };
+  },
+  directives: {
+    mask: VueTheMask.mask,
+  },
+  async mounted() {
+    const res = await fetch('./combined_countries_with_masks_filled.json');
+    this.countries = await res.json();
+    this.setMask();
+  },
+  methods: {
+    onCountryChange() {
+      this.setMask();
+      this.$emit("update:country", this.selectedCountryCode);
+    },
+    setMask() {
+      const country = this.countries.find(c => c.code === this.selectedCountryCode);
+      this.currentMask = country?.mask || "####################";
+    },
+  },
+  watch: {
+    phone(val) {
+      this.$emit("update:modelValue", val);
+    },
+  },
+  template: `
+    <div class="phone-input">
+      <select v-model="selectedCountryCode" @change="onCountryChange">
+        <option v-for="country in countries" :key="country.code" :value="country.code">
+          {{ country.flag }} {{ country.dial_code }}
+        </option>
+      </select>
+      <input
+        type="text"
+        v-model="phone"
+        v-mask="currentMask"
+        placeholder="Ingrese su número"
+      />
+    </div>
+  `,
+};
